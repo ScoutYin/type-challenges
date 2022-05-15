22dd445354efe9269a1a67d20fc7227e0f2002f9
ScoutYin <scoutyin.1900@gmail.com>
2022-05-15 20:18:47 +0800

    chore: prettier error -> warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    es2021: true
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-vars': 0
-  },
-  overrides: [
-    {
-      files: ['questions/**/*.ts'],
-      rules: {
-        // for '@ts-expect-error'
-        '@typescript-eslint/ban-ts-comment': 0
-      }
-    }
-  ]
+	root: true,
+	env: {
+		node: true,
+		es2021: true
+	},
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended'
+	],
+	rules: {
+		'@typescript-eslint/no-unused-vars': 0,
+		'prettier/prettier': 'warn'
+	},
+	overrides: [
+		{
+			files: ['questions/**/*.ts'],
+			rules: {
+				// for '@ts-expect-error'
+				'@typescript-eslint/ban-ts-comment': 0,
+				'@typescript-eslint/no-explicit-any': 'off'
+			}
+		}
+	]
 };

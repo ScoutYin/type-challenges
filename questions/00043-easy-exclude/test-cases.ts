2302d819697b893a9f6f3e215a46f6f3052b0fb8
ScoutYin <scoutyin.1900@gmail.com>
2022-05-19 22:06:02 +0800

    add 00043-easy-exclude

---
+++ questions/00043-easy-exclude/test-cases.ts
@@ -0,0 +1,18 @@
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+	Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, Exclude<'a' | 'b' | 'c', 'a'>>>,
+	Expect<
+		Equal<
+			MyExclude<'a' | 'b' | 'c', 'a' | 'b'>,
+			Exclude<'a' | 'b' | 'c', 'a' | 'b'>
+		>
+	>,
+	Expect<
+		Equal<
+			/* eslint-disable @typescript-eslint/ban-types */
+			MyExclude<string | number | (() => void), Function>,
+			Exclude<string | number | (() => void), Function>
+		>
+	>
+];

b1df7137aa11cb7a9dcd3fd966f5eef240b0c541
ScoutYin <scoutyin.1900@gmail.com>
2022-05-23 15:33:39 +0800

    docs: 添加注释

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,13 +1,21 @@
+/**
+ * 是否相等
+ */
 type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
 	? 1
 	: 2
 	? true
 	: false;
 
+/**
+ * 知识点：
+ * 1. 类型推断 infer
+ * 2. spread 操作符
+ * 3. Equal<X, Y>实现，如何判断两个类型相同
+ * 4. 递归
+ */
 type Includes<T extends readonly any[], U> = T extends [infer F, ...infer R]
 	? Equal<F, U> extends true
 		? true
 		: Includes<R, U>
 	: false;
-
-type a = Includes<['a', 'b', 1], 1>;
